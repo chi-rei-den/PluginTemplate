fc6487e82f4169f8ea52804a9f47200f0520fa7b
SGKoishi <i@sgkoi.dev>
2020-05-19 14:03:44 -0700

    Add project files and CI

---
+++ src/PluginTemplate/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+
+namespace Plugin
+{
+    [ApiVersion(2, 1)]
+    public class Plugin : TerrariaPlugin
+    {
+        public override string Author => "Author";
+
+        public override string Description => "Description";
+
+        public override string Name => "Plugin Name";
+
+        public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+
+        protected Plugin(Main game) : base(game)
+        {
+        }
+
+        public override void Initialize()
+        {
+            Commands.ChatCommands.Add(new Command(
+                permissions: new List<string> { "plugin.permission1", "plugin.permission2", },
+                cmd: this.Cmd,
+                "command1", "alias1", "alias2"));
+        }
+
+        private void Cmd(CommandArgs args)
+        {
+            args.Player.SendSuccessMessage("Welcome to your first command!");
+        }
+    }
+}
